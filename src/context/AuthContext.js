d45e8c4ebb724c9c4139463c7c3b889120bf8fe0
bekirbali <bfbali43@gmail.com>
2023-03-17 20:26:44 +0300

    login register

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,13 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../auth/firebase";
 
 export const AuthContext = createContext();
 
@@ -7,7 +16,65 @@ export const useAuthContext = () => {
 };
 
 const AuthContextProvider = ({ children }) => {
-  const values = {};
+  const [currentUser, setCurrentUser] = useState("");
+  console.log(currentUser);
+  const navigate = useNavigate();
+  useEffect(() => {
+    observeUser();
+  }, []);
+  // ! SIGN UP
+  const createUser = async (email, password, displayName) => {
+    try {
+      let user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(user);
+      console.log(displayName);
+      updateProfile(auth.currentUser, {
+        displayName: displayName,
+      });
+      console.log(user.displayName);
+      navigate("/");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  //! SIGN UP
+
+  // ! SIGN IN
+  const signInUser = async (email, password) => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const logoutUser = async () => {
+    signOut(auth);
+    navigate("/login");
+    setCurrentUser("");
+  };
+
+  const observeUser = async () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        setCurrentUser(user.displayName);
+      } else {
+        console.log("user signed out");
+      }
+    });
+  };
+  // ! SIGN IN
+
+  const values = {
+    createUser,
+    signInUser,
+    observeUser,
+    logoutUser,
+    currentUser,
+    setCurrentUser,
+  };
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
